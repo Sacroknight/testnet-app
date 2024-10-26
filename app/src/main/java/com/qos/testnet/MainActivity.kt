d7ebab12b73677ae3d3312d6a6d5f71764ad70e0
jorge <jorgefajardo_01@hotmail.com>
2024-10-25 21:23:14 -0500

    Integrate Firebase anonymous authentication
    Integrated Firebase anonymous authentication to assign unique user IDs. This involves signing in users anonymously upon app launch and storing their unique ID in
     DataStore preferences for persistent identification.

--- app/src/main/java/com/qos/testnet/MainActivity.kt
+++ app/src/main/java/com/qos/testnet/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.qos.testnet
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.qos.testnet.databinding.ActivityMainBinding
 import com.qos.testnet.permissionmanager.PermissionPreferences
 import com.qos.testnet.permissionmanager.RequestPermissions
@@ -17,12 +22,12 @@ class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
     private var requestPermissions: RequestPermissions? = null
     private val permissionPreferences: PermissionPreferences by lazy { PermissionPreferences.getInstance() }
-
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
-
+        mAuth = FirebaseAuth.getInstance()
         requestPermissions = RequestPermissions(this)
 
         CoroutineScope(Dispatchers.Main).launch {
@@ -41,6 +46,23 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        if (mAuth.currentUser == null) {
+            singInAnonymously()
+        } else {
+            val user: FirebaseUser? = mAuth.currentUser
+            user?.let {
+                Log.d("MainActivity", "User already signed in, userId: ${it.uid}")
+            }
+        }
+
+        lifecycleScope.launch {
+            val usrId = permissionPreferences.getUserId(
+                this@MainActivity,
+                PermissionPreferences.PermissionPreferencesKeys.USER_ID
+            )
+            Log.d("PreferencesStored", "Collected user Id: $usrId")
+        }
+
         val navView = findViewById<BottomNavigationView>(R.id.nav_view)
         val navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main)
 
@@ -54,5 +76,28 @@ class MainActivity : AppCompatActivity() {
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
         NavigationUI.setupWithNavController(navView, navController)
+    }
+
+    private fun singInAnonymously() {
+        mAuth.signInAnonymously()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user: FirebaseUser? = mAuth.currentUser
+                    user?.let {
+                        lifecycleScope.launch {
+                            permissionPreferences.saveUserId(
+                                this@MainActivity,
+                                PermissionPreferences.PermissionPreferencesKeys.USER_ID, it.uid
+                            )
+                            Log.d("MainActivity", "SingInAnonymously:success, user ID:  ${it.uid}")
+                        }
+
+                    }
+                } else {
+                    Log.w("MainActivity", "SingInAnonymously:failure", task.exception)
+                    Toast.makeText(this@MainActivity, "Authentication failed", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
     }
 }
--- app/src/main/java/com/qos/testnet/permissionmanager/PermissionPreferences.kt
+++ app/src/main/java/com/qos/testnet/permissionmanager/PermissionPreferences.kt
@@ -5,6 +5,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
@@ -14,12 +15,25 @@ class PermissionPreferences private constructor() {
     private val Context.permissionDataStore: DataStore<Preferences> by preferencesDataStore(name = "permission_preferences")
 
     object PermissionPreferencesKeys {
-        val DONT_ASK_AGAIN_LOCATION_PERMISSION = booleanPreferencesKey("dont_ask_again_location_permission")
+        val DONT_ASK_AGAIN_LOCATION_PERMISSION =
+            booleanPreferencesKey("dont_ask_again_location_permission")
         val ASK_OPEN_PERMISSION = booleanPreferencesKey("ask_open_permission")
-        val DONT_ASK_AGAIN_PHONE_PERMISSION = booleanPreferencesKey("dont_ask_again_phone_permission")
+        val DONT_ASK_AGAIN_PHONE_PERMISSION =
+            booleanPreferencesKey("dont_ask_again_phone_permission")
+        val USER_ID = stringPreferencesKey("User ID")
     }
 
-    suspend fun savePermissionPreference(context: Context, key: Preferences.Key<Boolean>, value: Boolean) {
+    suspend fun savePermissionPreference(
+        context: Context,
+        key: Preferences.Key<Boolean>,
+        value: Boolean
+    ) {
+        context.permissionDataStore.edit { preferences ->
+            preferences[key] = value
+        }
+    }
+
+    suspend fun saveUserId(context: Context, key: Preferences.Key<String>, value: String) {
         context.permissionDataStore.edit { preferences ->
             preferences[key] = value
         }
@@ -28,6 +42,12 @@ class PermissionPreferences private constructor() {
     suspend fun getPermissionPreference(context: Context, key: Preferences.Key<Boolean>): Boolean {
         return context.permissionDataStore.data.map { preferences ->
             preferences[key] ?: false
+        }.first()
+    }
+
+    suspend fun getUserId(context: Context, key: Preferences.Key<String>): String {
+        return context.permissionDataStore.data.map { preferences ->
+            preferences[key] ?: "User not found"
         }.first()
     }
 
